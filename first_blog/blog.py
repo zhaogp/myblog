8f14203a8ad1b8173eb3ad45dc255dbac9da91c0
zhaoguoping <zhaoguoping@jd.com>
2016-10-11 18:45:01 +0800

    连接上mysql数据库，写日志报错 'list' object has no attribute 'keys'

--- first_blog/blog.py
+++ first_blog/blog.py
@@ -2,12 +2,14 @@ from flask import Flask, g, url_for, render_template, request, redirect, session
 import os
 import click
 import sqlite3
+from first_blog.database import db_session, initdb
 
 app = Flask(__name__)
 
 #配置信息
 app.config.update(dict(
-	DATABASE=os.path.join(app.root_path, 'myblog.db'),
+	# DATABASE=os.path.join(app.root_path, 'myblog.db'),
+	SQLALCHEMY_DATABASE_URI='mysql://admin:11111@localhost:5005/blog',
 	USERNAME='admin',
 	PASSWORD='11111',
 	SECRET_KEY='key',
@@ -31,10 +33,7 @@ def get_db():
 
 def init_db():
 	with app.app_context():
-		db = get_db()
-		with app.open_resource('schema.sql', mode='r') as f:
-			db.cursor().executescript(f.read())
-		db.commit()
+		initdb()
 
 @click.command()
 def initdb_command():
@@ -47,14 +46,14 @@ def run_command():
 
 @app.route('/')
 def show_blogs():
-	db = get_db()
+	db = db_session
 	cur = db.execute('select title, content from blog order by id desc')
 	blogs = cur.fetchall()
 	return render_template('index.html', entries=blogs)
 
 @app.route('/add', methods=['POST'])
 def add_blog():
-	db = get_db()
+	db = db_session
 	db.execute('insert into blog(title, content) values(?, ?)', 
 				[request.form['title'], request.form['content']])
 	db.commit()
